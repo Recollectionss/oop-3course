6bf5dc70a13c8ccd8af39a4d00cc6dd5a8692831
Observersss <heavyobjectics@gmail.com>
2024-11-06 22:46:46 +0200

    writed good output for sax

--- lab-2/src/main/java/parsers/sax/UserHandler.java
+++ lab-2/src/main/java/parsers/sax/UserHandler.java
@@ -4,17 +4,60 @@ import org.xml.sax.SAXException;
 import org.xml.sax.Attributes;
 import org.xml.sax.helpers.DefaultHandler;
 
+import java.util.Objects;
 
 public class UserHandler extends DefaultHandler {
-    public void startElement( String uri, String localName, String qName, Attributes attributes)
-            throws SAXException {
-        System.out.println("Start Element : " + qName);
+    private StringBuilder buffer = new StringBuilder();
+    private int indentLevel = 0;
+
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        if (Objects.equals(qName, "root")) {
+            System.out.println("Start Element: " + qName);
+        } else {
+           if(Objects.equals(qName, "candy")) {
+               indentLevel++;
+               printIndent();
+               System.out.print(qName + ": ");
+               System.out.print('\n');
+               indentLevel++;
+           }else if(Objects.equals(qName, "value") || Objects.equals(qName, "ingredients")){
+               printIndent();
+               System.out.print(qName + ": ");
+               System.out.print('\n');
+               indentLevel++;
+           }else{
+               printIndent();
+               System.out.print(qName + ": ");
+               indentLevel++;
+           }
+        }
     }
 
     public void endElement(String uri, String localName, String qName) {
-        System.out.println("End Element : " + qName);
+        if (Objects.equals(qName, "root")) {
+            System.out.println("End Element: " + qName);
+        } else {
+
+            if (buffer.length() > 0) {
+                System.out.println(buffer.toString().trim());
+            }
+            buffer.setLength(0);
+            if(Objects.equals(qName, "candy")){
+                indentLevel-=2;
+            }else{
+                indentLevel--;
+            }
+        }
     }
+
     public void characters(char[] ch, int start, int length) {
-        System.out.println("Text Content : " + new String(ch, start, length));
+        buffer.append(new String(ch, start, length));
+    }
+
+    private void printIndent() {
+        for (int i = 0; i < indentLevel; i++) {
+            System.out.print("    ");
+        }
     }
 }
+
